51586b73dec75df81795c5cd5db7221d1bd750ac
maruf-git <munna.csehstu@gmail.com>
2025-10-15 11:42:19 +0600

    added cp template written in python

---
+++ 01_python/cf_problem_solve_with_python/cp_template.py
@@ -0,0 +1,19 @@
+# Codeforces Python Template
+
+import sys
+input = sys.stdin.readline   # fast input
+
+
+t = int(input()) # multiple test cases
+for _ in range(t):
+    
+    n = int(input()) # single integers
+    arr = list(map(int, input().split()))   # array input using map
+    a, b = map(int, input().split()) # two integers in one line using map
+    
+    # arr = [int(x) for x in input().split()] # array input using list comprehesion
+    # a, b = [int(x) for x in input().split()] # two integers in one line using list comprehesion
+    
+    # solution code here
+    
+    
